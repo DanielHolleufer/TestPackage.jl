cfe3869b796452056885f3ce193b7f07555f345f
Daniel Holleufer <daniel.holleufer@gmail.com>
2025-03-19 01:06:08 +0100

    plus_two

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -4,12 +4,4 @@ export plus_one
 
 greet() = print("Hello World!")
 
-"""
-    plus_one(x)
-
-Add one to the argument `x`.
-"""
-
-plus_one(x) = x + 1
-
 end # module TestPackage
---
+++ src/features.jl
@@ -0,0 +1,13 @@
+"""
+    plus_one(x)
+
+Add one to the argument `x`.
+"""
+plus_one(x) = x + 1
+
+"""
+    plus_two(x)
+
+Add one to the argument `x`.
+"""
+plus_two(x) = x + 2
